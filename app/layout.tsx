2124190b398bc1433a495c30428cd28cbea9f060
vitalik <vitalgreg1988@gmail.com>
2025-01-03 14:13:22 +0200

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppProviders } from "@/components/providers/AppProviders";
+import {
+  ClerkProvider,
+
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider afterSignOutUrl={'/sign-in'} appearance={{
+      elements:{
+        formButtonPrimary:"bg-primary hover:bg-primary/90 text-sm !shadow-none"
+      }
+    }}>
     <html lang="en">
       <body className={inter.className}>
           <AppProviders>
@@ -24,5 +33,6 @@ export default function RootLayout({
           </AppProviders>
        </body>
     </html>
+    </ClerkProvider>
   );
 }
