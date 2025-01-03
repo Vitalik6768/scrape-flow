d2f45da3dfd19dec21753a158f51bfc8fe6f97ec
vitalik <vitalgreg1988@gmail.com>
2025-01-03 12:59:21 +0200

    finish to add dark mode

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,29 @@
 import BreadCrumbHeader from '@/components/BreadCrumbHeader'
+import { AppProviders } from '@/components/providers/AppProviders'
 import DesktopDidebar from '@/components/Sidebar'
+import { ModeToggle } from '@/components/ThemeModeToggle'
 import { Separator } from '@/components/ui/separator'
 import React from 'react'
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className=' flex h-screen'>
-      <DesktopDidebar/>
+      <DesktopDidebar />
       <div className='flex flex-col flex-1 min-h-screen'>
         <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
-          <BreadCrumbHeader/>
-      </header>
+          <BreadCrumbHeader />
+          <div className='gap-1 flex-items-center'>
+            <ModeToggle/>
+
+          </div>
+        </header>
 
         <Separator />
         <div className='overflow-auto'>
           <div className='flex-1 container py-4 text-accent-foreground'>
-            {children}
+          
+              {children}
+
           </div>
         </div>
       </div>
